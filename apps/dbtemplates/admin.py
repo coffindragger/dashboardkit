94b49fad3b30065f8e4c90c0c882b51bbe99a1cc
Wiggins <wiggins@concentricsky.com>
2011-10-27 09:14:24 -0700

    jingo templates

--- apps/dbtemplates/admin.py
+++ apps/dbtemplates/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
+from django import forms
+from django.db import models
 
 from dbtemplates.models import DatabaseTemplate
 
-admin.site.register(DatabaseTemplate)
+
+class DatabaseTemplateAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': forms.Textarea(attrs={'rows':40,'cols':100})},
+    }
+admin.site.register(DatabaseTemplate, DatabaseTemplateAdmin)
--- apps/dbtemplates/loaders.py
+++ apps/dbtemplates/loaders.py
@@ -2,6 +2,7 @@ from django.template.loader import BaseLoader
 from django.template.base import TemplateDoesNotExist
 
 from dbtemplates.models import DatabaseTemplate
+from jingo import Template as JingoTemplate
 
 class DatabaseTemplateLoader(BaseLoader):
     is_usable = True
@@ -9,7 +10,7 @@ class DatabaseTemplateLoader(BaseLoader):
     def load_template_source(self, template_name, template_dirs=None):
         try:
             dbtemplate = DatabaseTemplate.objects.get(template_name=template_name)
-            return (dbtemplate.template_source, dbtemplate)
+            return JingoTemplate(dbtemplate.template_source), dbtemplate
         except DatabaseTemplate.DoesNotExist as e:
             pass
 
